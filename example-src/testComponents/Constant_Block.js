1bda845d796aa1bd2fd9a8c16df5235fdf7f1a83
liza <lisaminakova@gmail.com>
2020-04-11 00:06:33 +0300

    Вернула сборку вебпак.

--- example-src/testComponents/Constant_Block.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Constant_Block extends Component {
-  render() {
-    return <div className="constant-block">{this.props.constantTitle}</div>;
-  }
-}
-
-export default Constant_Block;
